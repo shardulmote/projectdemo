8a22eca1b0938f4ca5eb94ec17e55742a6420140
shardulmote <shardul.mote@atos.net>
2017-06-12 23:05:00 +0530

    Add files via upload

---
+++ admin/includes/footer.inc.php
@@ -0,0 +1,3 @@
+<div id="footer-wrap">
+	<p id="legal">(c) 2016 OurSite. Design by Diksha and Group.</p>
+	</div>
---
+++ admin/includes/logo.inc.php
@@ -0,0 +1,2 @@
+<h1>E-Book Library</h1>
+	<h2>Designed By Diksha Afreen Ritika Akshata</h2>
