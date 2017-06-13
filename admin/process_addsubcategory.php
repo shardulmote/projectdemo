94c0eb961cf455d1c4e7dd99cd75857a6eda5fa4
shardulmote <shardul.mote@atos.net>
2017-06-13 06:48:15 +0200

    Create process_addsubcategory.php

--- admin/process_addsubcategory.php
+++ admin/process_addsubcategory.php
@@ -1,1 +1,44 @@
-<?php	if(!empty($_POST))	{		$msg=array();		if(empty($_POST['subcat']) || empty($_POST['parent']))		{			$msg[]="Please full fill all requirement";		}				if(!empty($msg))		{			echo '<b>Error:-</b><br>';						foreach($msg as $k)			{				echo '<li>'.$k;			}		}		else		{					$link=mysql_connect("localhost","root","")or die("Can't Connect...");						mysql_select_db("shop",$link) or die("Can't Connect to Database...");					$parent = $_POST['parent'];			$subcat=$_POST['subcat'];						$query="insert into subcat(parent_id, subcat_nm) values('$parent','$subcat')";						mysql_query($query,$link) or die("can't Execute...");						mysql_close($link);			header("location:category.php");		}	}	else	{		header("location:index.php");	}?>		
+<?php
+
+	if(!empty($_POST))
+	{
+		$msg=array();
+		if(empty($_POST['subcat']) || empty($_POST['parent']))
+		{
+			$msg[]="Please full fill all requirement";
+		}
+		
+		if(!empty($msg))
+		{
+			echo '<b>Error:-</b><br>';
+			
+			foreach($msg as $k)
+			{
+				echo '<li>'.$k;
+			}
+		}
+		else
+		{
+		
+			$link=mysqli_connect("mysql.gebook1.svc","shardul","mote")or die("Can't Connect...");
+			
+			mysqli_select_db($link,"shop") or die("Can't Connect to Database...");
+		
+			$parent = $_POST['parent'];
+			$subcat=$_POST['subcat'];
+			
+			$query="insert into subcat(parent_id, subcat_nm) values('$parent','$subcat')";
+			
+			mysqli_query($link,$query) or die("can't Execute...");
+			
+			mysqli_close($link);
+			header("location:category.php");
+		}
+	}
+	else
+	{
+		header("location:index.php");
+	}
+?>
+	
+	
