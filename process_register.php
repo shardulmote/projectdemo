9699428fb8e5b3a4de21ccf6036d2726de3e6684
shardulmote <shardul.mote@atos.net>
2017-06-13 07:32:40 +0200

    Create process_register.php

--- process_register.php
+++ process_register.php
@@ -14,11 +14,11 @@
 			$msg.="<li>Please Enter your Password Again.....";
 		}
 		
-		if(!eregi("^[a-z0-9_]+[a-z0-9_.]*@[a-z0-9_-]+[a-z0-9_.-]*\.[a-z]{2,5}$",$_POST['mail']))
+		/*if(!eregi("^[a-z0-9_]+[a-z0-9_.]*@[a-z0-9_-]+[a-z0-9_.-]*\.[a-z]{2,5}$",$_POST['mail']))
 		{
 			$msg.="<li>Please Enter Your Valid Email Address...";
 		}
-		
+		*/
 		
 		if(strlen($_POST['pwd'])>10)
 		{
